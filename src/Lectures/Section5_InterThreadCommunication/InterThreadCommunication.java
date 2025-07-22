0f075870741c17c07f6e413f0e01506c190afc7b
Bajikomara <kbaji52@gmail.com>
2025-07-22 23:34:25 +0530

    20. Wait and notify()

--- src/Lectures/Section5_InterThreadCommunication/InterThreadCommunication.java
+++ src/Lectures/Section5_InterThreadCommunication/InterThreadCommunication.java
@@ -1,36 +1,59 @@
 package Lectures.Section5_InterThreadCommunication;
 
-class Process{
-    public void produce() throws InterruptedException{
-        synchronized(this){
-            System.out.println("Running the produce Method....");
-            wait();
-            System.out.println("Again Inside Process produce Method");
+import java.util.ArrayList;
+import java.util.List;
+
+class Processor{
+    private List<Integer> list = new ArrayList<Integer>();
+    private static final int UPPER_LIMIT = 5;
+    private static final int LOWER_LIMIT = 0;
+    private final Object lock = new Object();
+    private int value = 0;
+    public void producer() throws InterruptedException{
+        synchronized (lock){
+            while(true){
+                if(list.size() == UPPER_LIMIT){
+                    System.out.println("Waiting for removing items");
+                    lock.wait();
+                }else {
+                    System.out.println("Adding new item: " + value);
+                    list.add(value);
+                    value++;
+                    lock.notify();
+                }
+                Thread.sleep(1000);
+            }
         }
     }
 
-    public void consume() throws InterruptedException{
-        Thread.sleep(1000);
-        synchronized(this){
-            System.out.println("consume Method is executed.......");
-            notify();
-            //it is not going to handle the lock: we can make fuurthet operations
-            Thread.sleep(3000);
-        }
+    public void consumer() throws InterruptedException{
+        synchronized (lock){
+            while(true){
+                if(list.size() == LOWER_LIMIT){
+                    System.out.println("Waiting for adding items");
+                    value = 0;
+                    lock.wait();
+                }else {
+                    System.out.println("removing items: " + list.remove(list.size()-1));
+                    lock.notify();
+                }
+                Thread.sleep(1000);
+            }
 
+        }
     }
 }
 
 public class InterThreadCommunication {
 
     public static void main(String[] args) {
-        Process p = new Process();
+        Processor p = new Processor();
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
 
                 try {
-                    p.produce();
+                    p.producer();
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
@@ -41,7 +64,7 @@ public class InterThreadCommunication {
             @Override
             public void run() {
                 try {
-                    p.consume();
+                    p.consumer();
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
