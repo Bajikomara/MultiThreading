353d100a351ed6763c47fe82ec05d1e2c72e11e5
Bajikomara <kbaji52@gmail.com>
2025-07-22 21:42:34 +0530

    17.Problems with synchronization

--- src/Lectures/Section5_InterThreadCommunication/InterThreadCommunication.java
+++ src/Lectures/Section5_InterThreadCommunication/InterThreadCommunication.java
@@ -1,10 +1,26 @@
 package Lectures.Section5_InterThreadCommunication;
 
 public class InterThreadCommunication {
-    public static Integer counter = 0;
-    //we have to make sure this method is called only by a single thread at a given time
-    public static synchronized void increement(){
-        counter++;
+    public static Integer counter1 = 0;
+    public static Integer counter2 = 0;
+    //because App object has a single lock: this why the methods
+    // cannot be executed "at the same time"-time slicing algorithm
+//    public static synchronized void increement1(){
+//        counter1++;
+//    }
+
+    public static  void increement1(){
+//        class level locking
+//        rule of thumb: we synchronize blocks that are 100% necessary
+        synchronized (InterThreadCommunication.class){
+            counter1++;
+        }
+    }
+
+    public static synchronized void increement2(){
+        synchronized (InterThreadCommunication.class){
+            counter2++;
+        }
     }
 
     public static void process(){
@@ -12,7 +28,7 @@ public class InterThreadCommunication {
             @Override
             public void run() {
                 for(int i = 0; i<100; i++){
-                    increement();
+                    increement1();
                 }
             }
         });
@@ -20,7 +36,7 @@ public class InterThreadCommunication {
             @Override
             public void run() {
                 for(int i = 0; i<100; i++){
-                    increement();
+                    increement2();
                 }
             }
         });
@@ -36,7 +52,8 @@ public class InterThreadCommunication {
             throw new RuntimeException(e);
         }
 
-        System.out.println("The counter is: " + counter);
+        System.out.println("The counter is: " + counter1);
+        System.out.println("The counter is: " + counter2);
     }
 
     public static void main(String[] args) {
