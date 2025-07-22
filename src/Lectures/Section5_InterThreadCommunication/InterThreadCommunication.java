caf8bc7e94883f4a70fece17d3e48bcd952fbfae
Bajikomara <kbaji52@gmail.com>
2025-07-22 22:47:28 +0530

    20. Wait and notify()

--- src/Lectures/Section5_InterThreadCommunication/InterThreadCommunication.java
+++ src/Lectures/Section5_InterThreadCommunication/InterThreadCommunication.java
@@ -1,58 +1,55 @@
 package Lectures.Section5_InterThreadCommunication;
 
+class Process{
+    public void produce() throws InterruptedException{
+        synchronized(this){
+            System.out.println("Running the produce Method....");
+            wait();
+            System.out.println("Again Inside Process produce Method");
+        }
+    }
+
+    public void consume() throws InterruptedException{
+        Thread.sleep(1000);
+        synchronized(this){
+            System.out.println("consume Method is executed.......");
+            notify();
+            //it is not going to handle the lock: we can make fuurthet operations
+            Thread.sleep(3000);
+        }
+
+    }
+}
+
 public class InterThreadCommunication {
-    public static Integer counter1 = 0;
-    public static Integer counter2 = 0;
 
-    private static final Object lock1 = new Object();
-    private static final Object lock2 = new Object();
-
-    public static  void increement1(){
-        synchronized (lock1){
-            counter1++;
-        }
-    }
-    public static synchronized void increement2(){
-        synchronized (lock2){
-            counter2++;
-        }
-    }
-
-    public static void process(){
+    public static void main(String[] args) {
+        Process p = new Process();
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for(int i = 0; i<100; i++){
-                    increement1();
+
+                try {
+                    p.produce();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
         });
+
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for(int i = 0; i<100; i++){
-                    increement2();
+                try {
+                    p.consume();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
         });
 
         t1.start();
         t2.start();
-
-
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        System.out.println("The counter is: " + counter1);
-        System.out.println("The counter is: " + counter2);
-    }
-
-    public static void main(String[] args) {
-        process();
 
     }
 
