cddd282e7089a0fd6399f3781a40ef005e1005dd
Bajikomara <kbaji52@gmail.com>
2025-07-23 23:59:53 +0530

    .ExecutorsExample-Fixed-Thread-Pool

---
+++ src/Lectures/section7_Executors/FixedThreadExecutors.java
@@ -0,0 +1,31 @@
+package Lectures.section7_Executors;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+class Work implements Runnable {
+    private int id;
+    public Work(int id) {
+        this.id = id;
+    }
+    @Override
+    public void run() {
+        System.out.println("Task with id " + id + " is in work-thread id: " + Thread.currentThread().getId());
+        long duration = (long) Math.random()*5;
+        try {
+            TimeUnit.SECONDS.sleep(duration);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
+
+public class FixedThreadExecutors {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 10; i++) {
+            executor.execute(new Work(i + 1));
+        }
+    }
+}
